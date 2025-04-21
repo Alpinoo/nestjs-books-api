0e641fe255a9128a238f2e3ecb5153d954a5ccbc
Alp Oral <alp.oral97@gmail.com>
2025-04-22 01:24:52 +0300

    books contoller&service implemented

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { BooksService, Book } from './books.service';
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  getBooks(): Book[] {
+    return this.booksService.getAllBooks();
+  }
+
+  @Post()
+  createBook(
+    @Body('title') title: string,
+    @Body('author') author: string,
+  ): Book {
+    return this.booksService.createBook(title, author);
+  }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+}
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  private books: Book[] = [];
+
+  //GET books
+  getAllBooks(): Book[] {
+    return this.books;
+  }
+
+  createBook(title: string, author: string) {
+    const newBook: Book = {
+      id: this.books.length + 1,
+      title,
+      author,
+    };
+
+    this.books.push(newBook);
+
+    return newBook;
+  }
+}
