74a9b208bd676658aa1088509c9c7aa87314c91b
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2025-04-25 20:54:14 +0300

    tests implemented

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -16,4 +16,54 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('creates a new book', () => {
+    const title = 'Book of Boba Fett';
+    const author = 'George Lucas';
+
+    const result = service.createBook(title, author);
+
+    expect(result).toBeDefined();
+    expect(result.title).toBe(title);
+    expect(result.author).toBe(author);
+    expect(result.id).toBe(1);
+
+    const allBooks = service.getAllBooks();
+
+    expect(allBooks).toContainEqual(result);
+  });
+
+  it('are books really added?', () => {
+    const allBooks = service.getAllBooks();
+
+    expect(allBooks).toHaveLength(0);
+
+    const booksToAdd = [
+      {
+        title: 'Booking',
+        author: 'Someone',
+      },
+      {
+        title: 'Cooking',
+        author: 'Walter',
+      },
+    ];
+
+    booksToAdd.forEach((book) => {
+      service.createBook(book.title, book.author);
+    });
+
+    const newAllBooks = service.getAllBooks();
+
+    expect(newAllBooks).toHaveLength(2);
+  });
+
+  it('logs book when book is created', () => {
+    const spy = jest.spyOn(LoggerService.prototype, 'warn');
+
+    service.createBook('titling', 'alpino');
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('A new book has been created');
+  });
 });
